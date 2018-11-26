95abd56942f46baab6259a88cbf84de9d38dd1a2
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2018-11-26 12:42:35 +0200

    tooltip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Button from "./components/button";
 import Form from "./components/form";
 import Menu from "./components/menu";
+import Tooltip from "./components/tooltip";
 
 class App extends React.Component{
     render(){
         return (
             <div>
                 <Menu />
+                <Tooltip text='Ты на меня навел!'>Наведи на меня!</Tooltip>
             </div>
         )
     }
