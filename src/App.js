6fdac748312158ef3e2cd11f4bad52f93cf7c81b
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2018-11-28 18:03:43 +0200

    to do list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Button from "./components/button";
-import Form from "./components/form";
-import Menu from "./components/menu";
-import Tooltip from "./components/tooltip";
-import Timer from "./components/timer";
+// import Button from "./components/button";
+// import Form from "./components/form";
+// import Menu from "./components/menu";
+// import Tooltip from "./components/tooltip";
+// import Timer from "./components/timer";
+import Todo from "./components/todo";
 
 class App extends React.Component{
     render(){
         return (
-            <div>
-                <Menu />
-                <Tooltip text='Ты на меня навел!'>Наведи на меня!</Tooltip>
-                <Timer />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-12">
+                        <Todo />
+                    </div>
+                </div>
             </div>
         )
     }
---
+++ src/components/todo.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "../components/todo.css";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheckCircle, faTrashAlt);
+
+class Todo extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            tasks: [
+                {
+                    oneTask: 'I need to call my boss',
+                    doneTask: false
+                },
+                {
+                    oneTask: 'Find the best way to solve a task',
+                    doneTask: true
+                },
+                {
+                    oneTask: 'Be happy',
+                    doneTask: true
+                }
+            ]
+        };
+    }
+
+    render(){
+
+        return (
+            <div className="todo-list">
+                <h3>What you need to do?</h3>
+                <div className="input-group mt-4">
+                    <span className="input-group-btn main-input">
+                        <input type="text" name="newTask" className="form-control" />
+                    </span>
+                    <button className="btn btn-primary main-button">Add task</button>
+                </div>
+
+                <div className="tasks-container mt-5">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Done</th>
+                                <th scope="col">Task</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td><FontAwesomeIcon icon="check-circle" className="custom-awasome-icon action-false" /></td>
+                                <td>I need to call my boss</td>
+                                <td><FontAwesomeIcon icon="trash-alt" className="custom-awasome-icon text-danger" /></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+export default Todo;
